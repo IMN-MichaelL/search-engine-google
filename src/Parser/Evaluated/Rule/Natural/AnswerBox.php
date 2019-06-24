b3c8224b65f8444de906b0e4d2e20a759f9f02c3
Michael Lauridsen <michaell@imninjas.com>
2019-06-24 12:20:06 -0400

    Updates matching logic for new side snippets

--- src/Parser/Evaluated/Rule/Natural/AnswerBox.php
+++ src/Parser/Evaluated/Rule/Natural/AnswerBox.php
@@ -18,8 +18,7 @@ class AnswerBox implements ParsingRuleInterface
     {
         $nodeClasses = explode(' ', $node->getAttribute('class'));
 
-        if (count($nodeClasses) == 3 &&
-            count(array_diff(array('g', 'mnr-c', 'g-blk'), $nodeClasses)) == 0
+        if (count(array_diff(array('g', 'mnr-c', 'g-blk'), $nodeClasses)) == 0
             && (
                 $dom->cssQuery('.ifM9O', $node)->length == 1 ||
                 $dom->cssQuery('._Z7', $node)->length == 1      // TODO used for BC, remove in the future
